e8e61019391b17e5676caf757265b4b46495e9cb
Manuel Salazar <manuel-alejandro.salazar@atos.net>
2022-05-11 14:44:26 -0500

    no http config

--- handler.go
+++ handler.go
@@ -2,27 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"os"
 )
 
-func simpleTimer(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("hello world")
-	message := "TIMER TRIGGER TESTING.\n"
-	name := r.URL.Query().Get("name")
-	if name != "" {
-		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
-	}
-	fmt.Fprint(w, message)
-}
-
 func main() {
-	customHandlerPort, exists := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT")
-	if exists {
-		fmt.Println("FUNCTIONS_CUSTOMHANDLER_PORT: " + customHandlerPort)
-	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/TimerExample", simpleTimer)
-	log.Fatal(http.ListenAndServe(":"+customHandlerPort, mux))
+	fmt.Println("hello world")
 }
