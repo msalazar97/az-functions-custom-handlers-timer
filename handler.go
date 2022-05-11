00101b3394086da56233ab88a5ca034e8868e5d9
Manuel Salazar <manuel-alejandro.salazar@atos.net>
2022-05-11 14:42:33 -0500

    http configuration

--- handler.go
+++ handler.go
@@ -7,21 +7,22 @@ import (
 	"os"
 )
 
-func timer(w http.ResponseWriter, r *http.Request) {
+func simpleTimer(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello world")
-	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
+	message := "TESTING TIMER TRIGGER \n"
 	name := r.URL.Query().Get("name")
 	if name != "" {
-		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
+		message = fmt.Sprintf("Hello, %s.\n", name)
 	}
 	fmt.Fprint(w, message)
 }
 
 func main() {
-	listenAddr := ":8080"
-	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-		listenAddr = ":" + val
+	customHandlerPort, exists := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT")
+	if exists {
+		fmt.Println("FUNCTIONS_CUSTOMHANDLER_PORT: " + customHandlerPort)
 	}
-	http.HandleFunc("/TimerExample", timer)
-	log.Fatal(http.ListenAndServe(listenAddr, nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/TimerExample", simpleTimer)
+	log.Fatal(http.ListenAndServe(":"+customHandlerPort, mux))
 }
